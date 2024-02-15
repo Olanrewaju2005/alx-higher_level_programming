9316031c6df8de07c2cdc0986d65f98a27c90d6e
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-02-15 10:30:45 -0800

    more query

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,11 @@
+-- script that creates the MySQL server user user_0d_1
+-- user_0d_1 will have all privileges on MySQL server
+-- If the user user_0d_1 already exists, script should not fail
+CREATE USER
+IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON *.*
+	TO 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
