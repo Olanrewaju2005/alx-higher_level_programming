584f7c723fa5cccefb7dece449ac8fee3e34bcec
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-02-15 08:09:58 -0800

    test commit

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,6 @@
 -- Script creates a table in the current database in my MySQL server
 -- Database name will be passed as an argument of tht mysql command
 -- Script will not fail if the table already exists
-USE `hbtn_0c_0`
-
 CREATE TABLE IF NOT EXISTS first_table(
 		id INT,
 		name VARCHAR(256)
