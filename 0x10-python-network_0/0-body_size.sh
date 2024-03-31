080f15d5c332b5bd79d7fd106574cc160548243d
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-03-31 23:36:01 +0100

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 if [ -z "$1" ]; then
     echo "Usage: $0 <url>"
     exit 1
