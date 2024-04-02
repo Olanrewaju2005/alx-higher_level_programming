6348514912040f34f025702b4d2ac9d7baabb5dd
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-04-02 05:13:32 +0100

    cURL to the end

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+
+url=$1
+response=$(curl -s -o response.txt -w "%{http_code}" "$url")
+
+if [ "$response" -eq 200 ]; then
+    cat response.txt
+fi
+rm -f response.txt
