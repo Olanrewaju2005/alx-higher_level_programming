426ea41960b9c8da55a9fe69f71505d4603753ee
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-04-02 03:11:21 +0100

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    """ script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter """
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    url = "http://0.0.0.0:5000/search_user"
+    param = {'q': letter}
+    response = requests.post(url, json=param)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            if data:
+                print(f"[{data['id']}] {data['name']}")
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
+    else:
+        pass
