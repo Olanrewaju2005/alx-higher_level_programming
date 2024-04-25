c171822d5a90b7e294809aa2193348e241c7f621
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-04-25 17:06:31 +0100

    star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// import the request module
+const request = require('request');
+
+// construct the URL for the specific Star wars film
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+// use the request module to perform an HTTP GET request
+// to the constructed URL
+request(url, function (error, response, body) {
+    //log title if succesful, log error if not
+    console.log(error || JSON.parse(body).title);
+    });
